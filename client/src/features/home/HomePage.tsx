905b57b2979ec4f0739260d3eaee446a475b09fa
filev-svt <filev.svt@gmail.com>
2023-10-10 23:41:27 +0200

    feat: error handling toying

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,5 +1,29 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import agent from "../../app/api/agent";
 
 export const HomePage = () => {
-  return <Typography variant="h1">Home</Typography>;
+  return (
+    <Container>
+      <Typography gutterBottom variant="h1">
+        Errors for testing purposes
+      </Typography>
+      <ButtonGroup>
+        <Button variant="contained" onClick={() => agent.TestErrors.get400()}>
+          Test 400 error
+        </Button>
+        <Button variant="contained" onClick={() => agent.TestErrors.get401()}>
+          Test 401 error
+        </Button>
+        <Button variant="contained" onClick={() => agent.TestErrors.get404()}>
+          Test 404 error
+        </Button>
+        <Button variant="contained" onClick={() => agent.TestErrors.get500()}>
+          Test 500 error
+        </Button>
+        <Button variant="contained" onClick={() => agent.TestErrors.get422()}>
+          Test 422 error
+        </Button>
+      </ButtonGroup>
+    </Container>
+  );
 };
